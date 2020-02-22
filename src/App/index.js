c30cf469738732f7e81615504ac95ec4096c617e
Divyendu Narayan <divyendu.narayan@gmail.com>
2020-02-22 11:08:59 -0800

    time series analyzer demo app

--- src/App/index.js
+++ src/App/index.js
@@ -1,11 +1,25 @@
 import React, { Component } from "react";
 import "./style.css";
-import {TimeSeriesAnalyzer} from 'time-series-analyzer-webapp';
+import {TimeSeriesAnalyzer, TimeSeriesAnalyzerTutorial} from 'time-series-analyzer-webapp';
+import {NavLink, Switch, Route} from 'react-router-dom';
 
-class App extends Component {
-  render() {
-    return <div className="App"><TimeSeriesAnalyzer/></div>;
-  }
-}
+const RenderTimeSeriesAnalyzer = () => (
+  <TimeSeriesAnalyzer/>
+);
+
+const RenderTimeSeriesAnalyzerTutorial = () => (
+  <TimeSeriesAnalyzerTutorial/>
+)
+
+export const App = () => (
+  <div className="App">
+    <NavLink exact activeClassName="current" to='/time-series-analyzer/application'><span className="app-button">application</span></NavLink>
+    <NavLink exact activeClassName="current" to='/time-series-analyzer/tutorial'><span className="app-button">tutorial</span></NavLink>
+    <Switch>
+      <Route exact path="/time-series-analyzer/application" component={RenderTimeSeriesAnalyzer}></Route>
+      <Route exact path="/time-series-analyzer/tutorial" component={RenderTimeSeriesAnalyzerTutorial}></Route>
+    </Switch>
+  </div>
+)
 
 export default App;
